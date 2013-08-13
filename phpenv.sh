fa0a07973d5a961a512d6fba0ca342e9c116419c
robbinhan <luckyhanbin@gmail.com>
2013-08-13 19:50:06 +0800

    安装个版本完成

--- phpenv.sh
+++ phpenv.sh
@@ -21,7 +21,6 @@ if [ ! -f "$save_file_name" ]; then
   `wget  "$url$prefix$version$ext" -O $save_file_name`;
 fi
 #unpack
-`cd /tmp`
 #判断解包目录是否存在
 echo "$save_file_name tar……"
 if [ ! -d "$save_file_path" ]; then
@@ -29,6 +28,12 @@ if [ ! -d "$save_file_path" ]; then
 fi
 #编译
 echo "Configure……"
-`cd $save_file_path`
-$save_file_path/configure --without-pear --with-gd --enable-sockets --with-jpeg-dir=/usr --with-png-dir=/usr --enable-exif --enable-zip --with-zlib --with-zlib-dir=/usr --with-kerberos --with-openssl --with-mcrypt=/usr --enable-soap --enable-xmlreader --with-xsl --enable-ftp --enable-cgi --with-curl=/usr --with-tidy --with-xmlrpc --enable-sysvsem --enable-sysvshm --enable-shmop --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-pdo-sqlite --enable-pcntl --with-libedit --enable-mbstring --disable-debug --enable-fpm --prefix=$install_dir
-#make && make install
+backup_pwd=$(pwd)
+cd $save_file_path
+if [ ! -f ./configure ]; then
+    ./buildconf
+fi
+pwd
+./configure --without-pear --with-gd --enable-sockets --with-jpeg-dir=/usr --with-png-dir=/usr --enable-exif --enable-zip --with-zlib --with-zlib-dir=/usr --with-kerberos --with-openssl --with-mcrypt=/usr --enable-soap --enable-xmlreader --with-xsl --enable-ftp --enable-cgi --with-curl=/usr --with-tidy --with-xmlrpc --enable-sysvsem --enable-sysvshm --enable-shmop --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-pdo-sqlite --enable-pcntl --with-libedit --enable-mbstring --disable-debug --enable-fpm --prefix=$install_dir
+make && make install
+cd $backup_pwd
