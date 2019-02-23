d19747b823e029f4294f9b51baeeffcfd60585d0
hanb <hanb@kingnet.com>
2019-02-23 23:19:00 +0800

    更新

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ PHP各版本安装，灵活切换
 
 [2013-10-25]
 支持断点续传下载 PHP 版本
+
+[2019-02-23]
+更新版本下载地址，去除陈旧版本，地址依赖官方页面：www.php.net/releases/ 都用的美国的镜像，cn的镜像不稳定 很多版本都没有了，无奈
+
+
+#安装
+1. clone当前仓库
+2. git submodule init
+3. git submodule update
+
+#使用
+
+```bash
+
+./phpenv.sh 5.6.40
+
+```
+
+
--- phpenv.sh
+++ phpenv.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+
+#获取版本
 version=$1;
+#configure 参数
+configure=$2;
+
 if [ ! -n "$version" ]; then
   echo "unknown php version ";
   exit;
@@ -11,13 +16,15 @@ config_file_name=$definitions_dir$version;
 http_file_path=`cat $config_file_name`
 save_file_name="/tmp/$version$ext";
 save_file_path="/tmp/$prefix$version";
+
+#判断安装目录是否创建
 install_dir="$HOME/.phpenv/versions/$version";
 if [ ! -d "$install_dir" ]; then
   echo "make install dir..."
   mkdir -p $install_dir;
 fi
 
-#这里的-f参数判断是否存在
+#这里的-f参数判断文件是否存在
 #download
 echo "$http_file_path  downloading……"
 if [ ! -f "$save_file_name" ]; then
@@ -36,7 +43,45 @@ cd $save_file_path
 if [ ! -f ./configure ]; then
     ./buildconf
 fi
+
+if [ ! -n "$configure"];then 
+configure="--without-pear \
+--with-gd \
+--enable-sockets \
+--with-jpeg-dir=/usr \
+--with-png-dir=/usr \
+--enable-exif \
+--enable-zip \
+--with-zlib \
+--with-zlib-dir=/usr \
+--with-kerberos \
+--with-openssl \
+--with-mcrypt=/usr \
+--enable-soap \
+--enable-xmlreader \
+--with-xsl \
+--enable-ftp \
+--enable-cgi \
+--with-curl=/usr \
+--with-tidy \
+--with-xmlrpc \
+--enable-sysvsem \
+--enable-sysvshm \
+--enable-shmop \
+--with-mysql=mysqlnd \
+--with-mysqli=mysqlnd \
+--with-pdo-mysql=mysqlnd \
+--with-pdo-sqlite \
+--enable-pcntl \
+--with-libedit \
+--enable-mbstring \
+--disable-debug \
+--enable-fpm \
+--prefix=$install_dir"
+fi
+
 pwd
-./configure --without-pear --with-gd --enable-sockets --with-jpeg-dir=/usr --with-png-dir=/usr --enable-exif --enable-zip --with-zlib --with-zlib-dir=/usr --with-kerberos --with-openssl --with-mcrypt=/usr --enable-soap --enable-xmlreader --with-xsl --enable-ftp --enable-cgi --with-curl=/usr --with-tidy --with-xmlrpc --enable-sysvsem --enable-sysvshm --enable-shmop --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-pdo-sqlite --enable-pcntl --with-libedit --enable-mbstring --disable-debug --enable-fpm --prefix=$install_dir
+./configure $configure
 make && make install
+
 cd $backup_pwd
